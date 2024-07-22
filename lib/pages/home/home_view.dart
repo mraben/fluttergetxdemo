96bc57edc96c726037f16fa992e0a0c87bccf21c
demo <demo@gmail.com>
2024-07-22 23:35:10 +0800

    首页列表

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:demoflutter/internationalization/globalization.dart';
 import 'package:flutter/material.dart';
 import 'package:demoflutter/common_ui/wb_cus_image_view.dart';
@@ -44,11 +46,7 @@ class HomePage extends StatelessWidget {
             onRefresh: logic.refreshData,
             header: CNHeader(),
             child: ListView(
-              children: [
-                _banner(),
-                _marqueeText(),
-                _getCenterText(),
-              ],
+              children: [_banner(), _marqueeText(), _getCenterText()],
             ),
           ),
         ),
@@ -59,7 +57,7 @@ class HomePage extends StatelessWidget {
   /// 轮播图
   Widget _banner() {
     return Container(
-      margin: const EdgeInsets.all(15),
+      margin: const EdgeInsets.fromLTRB(15, 15, 15, 10),
       height: Get.width * 0.4,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12), color: Colors.transparent),
@@ -80,7 +78,7 @@ class HomePage extends StatelessWidget {
               logic.launchURL(model.url.toString());
             },
             child: Container(
-                margin: const EdgeInsets.only(left: 5, right: 5),
+                margin: EdgeInsets.only(left: 5, right: 5),
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(12)),
@@ -98,20 +96,109 @@ class HomePage extends StatelessWidget {
   /// 跑马灯
   Widget _marqueeText() {
     return Container(
-      padding: const EdgeInsets.only(left: 15, right: 15),
-      margin: const EdgeInsets.only(top: 10, bottom: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(7), color: Colors.white),
+      padding: const EdgeInsets.only(left: 5, top: 5, right: 5, bottom: 5),
+      margin: const EdgeInsets.only(left: 15, bottom: 10, right: 15),
       child: SSMarquee(
-        child: Text(logic.homeMarqueeString.value.content ?? ""),
+        child: Text(
+          logic.homeMarqueeString.value.content ?? "",
+          style: const TextStyle(fontSize: 12),
+        ),
       ),
     );
   }
 
   ///list列表
   Widget _getCenterText() {
-    return Container(
-      margin: const EdgeInsets.only(top: 50),
-      alignment: Alignment.center,
-      child: Text(WBGlobalization.home.tr),
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: logic.homeListModel.value.newslist?.length,
+        itemBuilder: (child, index) {
+          return getItemView(index);
+        });
+  }
+
+  Widget getItemView(int position) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(13, 6, 13, 6),
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: Colors.white),
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                Text(
+                  logic.homeListModel.value.newslist![position].source
+                      .toString(),
+                  style: const TextStyle(fontSize: 13),
+                ),
+                const Spacer(),
+                Text(
+                  logic.homeListModel.value.newslist![position].ctime
+                      .toString(),
+                  style: const TextStyle(fontSize: 13),
+                ),
+              ],
+            ),
+            const Divider(
+              height: 5,
+            ),
+            Row(
+              children: [
+                WBCusImageView(
+                  icon: logic.homeListModel.value.newslist![position].picUrl
+                      .toString(),
+                  width: 120,
+                  height: 70,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            logic.homeListModel.value.newslist![position].title
+                                .toString(),
+                            textAlign: TextAlign.start,
+                            overflow: TextOverflow.ellipsis,
+                            softWrap: false,
+                            style: const TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            logic.homeListModel.value.newslist![position]
+                                .description
+                                .toString(),
+                            textAlign: TextAlign.left,
+                            overflow: TextOverflow.clip,
+                            softWrap: true,
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
